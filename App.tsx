f3b1cb7ec9684f48f36b35f79c5764f164ebdd31
mguibtech <mguibtech@gmail.com>
2023-08-08 15:35:12 -0400

    feat: Create project and add custom theme

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, StatusBar } from 'react-native';
+import { NativeBaseProvider,Text } from 'native-base'
+
+import { useFonts, Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla'
+import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
+
 
 export default function App() {
+
+  const [fonstsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle='dark-content'
+        backgroundColor='transparent'
+        translucent
+      />
+
+      {!fonstsLoaded ? <View/> : <Loading />}
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
